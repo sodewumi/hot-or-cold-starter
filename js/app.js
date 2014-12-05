850078ddbff3b505348b4a50408a02336aa9e7fb
distor <stellaodewumi@gmail.com>
2014-12-05 14:53:13 -0600

    finished guessing logic

--- js/app.js
+++ js/app.js
@@ -1,47 +1,60 @@
-$(document).ready(function(){
-	
-	/*--- Display information modal box ---*/
-  	$(".what").click(function(){
-    	$(".overlay").fadeIn(1000);
+$(document).ready(function () {
+    var rightNumber;
+  	//var guess;
+  
+  	//Display information modal box
+    $(".what").click(function(){
+    $(".overlay").fadeIn(1000);
 
-  	});
+    });
 
-  	/*--- Hide information modal box ---*/
-  	$("a.close").click(function(){
-  		$(".overlay").fadeOut(1000);
-  	});
-
-  	var rightNumber = Math.floor((Math.random() * 100) + 1)
-  	console.log(rightNumber);
-  	
-  	//when user starts a new game
-  	function newGame() {
-  		console.log(rightNumber);
-
-  	}
-
-  	function guesses() {
-  			console.log(rightNumber);
-  			// var guess = document.getElementById('userGuess').value();
-  			// if ( guess += 20 < rightNumber < guess -= 20) {
-  			// 	console.log('you\'re cold bro');
-  			// }
-  			var guess = document.getElementById('userGuess').value;
-  			var guess = parseInt(guess);
-
-  			console.log(guess, rightNumber);
+    //Hide information modal box
+    $("a.close").click(function(){
+      $(".overlay").fadeOut(1000);
+    });
 
 
-  	};
+    
+    //starts a new game
+    function newGame() {
 
-  	//guesses();
-  	newGame();
+      rightNumber = Math.floor((Math.random()*100)+1);    
+    };
 
+   function guesses() {
+		var guess = document.getElementById('userGuess').value;
+		guess = parseInt(guess);
 
-  	//starts new game on click
-  	$('.new').on('click', newGame);
-  	$('#guessButton').on('click', guesses);
+		var difference = rightNumber - guess;
+		if (difference < 0) {
+		  difference = difference * -1;
+		};
+
+		if (difference <= 5) {
+		  console.log('you\'re on fire');
+		} else if (difference <= 10) {
+		  console.log('you\'re very hot');
+		} else if (difference <= 20) {
+		  console.log('you\'re hot');
+		} else if (difference <= 30) {
+		  console.log('you\'re warm');
+		} else if (difference <= 50) {
+		  console.log('you\'re cold');
+		} else if (difference > 50) {
+		  console.log('you\'re ice cold');
+		}
+   };
+
+    newGame();
+    //console.log(rightNumber)
+    console.log(rightNumber);
+
+    //starts new game on click
+    $('.new').on('click', newGame);
+    
+    $('form').submit(function(e) {
+      e.preventDefault();
+      guesses();
+    });
 
 });
-
-
