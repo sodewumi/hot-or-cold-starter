8fd822d068740dac84b833d163b1b2701690367c
distor <stellaodewumi@gmail.com>
2014-12-05 13:22:50 -0600

    App generates a random number. User can enter guess. Clicking new game generates a new number

--- js/app.js
+++ js/app.js
@@ -1,4 +1,3 @@
-
 $(document).ready(function(){
 	
 	/*--- Display information modal box ---*/
@@ -11,6 +10,37 @@ $(document).ready(function(){
   	$("a.close").click(function(){
   		$(".overlay").fadeOut(1000);
   	});
+
+  	var rightNumber = Math.floor((Math.random() * 100) + 1)
+  	console.log(rightNumber);
+  	
+  	//when user starts a new game
+  	function newGame() {
+  		console.log(rightNumber);
+
+  	}
+
+  	function guesses() {
+  			console.log(rightNumber);
+  			// var guess = document.getElementById('userGuess').value();
+  			// if ( guess += 20 < rightNumber < guess -= 20) {
+  			// 	console.log('you\'re cold bro');
+  			// }
+  			var guess = document.getElementById('userGuess').value;
+  			var guess = parseInt(guess);
+
+  			console.log(guess, rightNumber);
+
+
+  	};
+
+  	//guesses();
+  	newGame();
+
+
+  	//starts new game on click
+  	$('.new').on('click', newGame);
+  	$('#guessButton').on('click', guesses);
 
 });
 
