25701f7d6f3850a53b73e80d2b99a837c463b61b
distor <stellaodewumi@gmail.com>
2014-12-05 16:53:36 -0600

    Can do validate a user's number and run game. I have not finished what the program will do if it doesn't validate

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
 $(document).ready(function () {
     var rightNumber;
-  	//var guess;
+  	var difference;
+  	var guess;
   
   	//Display information modal box
     $(".what").click(function(){
@@ -21,30 +22,65 @@ $(document).ready(function () {
       rightNumber = Math.floor((Math.random()*100)+1);    
     };
 
-   function guesses() {
-		var guess = document.getElementById('userGuess').value;
+    //checks for guess
+    function guesses() {
+		guess = document.getElementById('userGuess').value;
 		guess = parseInt(guess);
 
-		var difference = rightNumber - guess;
+		difference = rightNumber - guess;
 		if (difference < 0) {
 		  difference = difference * -1;
-		};
-
-		if (difference <= 5) {
-		  console.log('you\'re on fire');
-		} else if (difference <= 10) {
-		  console.log('you\'re very hot');
-		} else if (difference <= 20) {
-		  console.log('you\'re hot');
-		} else if (difference <= 30) {
-		  console.log('you\'re warm');
-		} else if (difference <= 50) {
-		  console.log('you\'re cold');
-		} else if (difference > 50) {
-		  console.log('you\'re ice cold');
+		  return difference;
+		} else {
+		  return (difference);
 		}
-   };
+		console.log('yo')
 
+    };
+
+    //checks if  the user's guess is a real number
+    var validation = function(x) {
+    	if (isNaN(x)) {
+    		//return [text('Please enter a real number'), false]
+    		//return (alert('yo'));
+    		return false;
+    	} else if ( x < 1 || x > 100) {
+    		//return text('Please enter a number between 1-100');
+    		return false;
+    	} else if (x == "") {
+    		return false;
+    	} else {
+    		return true;
+    	}
+
+    };
+
+    //gives the user feedback depending on the difference between the right number
+    //and the one the guessed.
+	function comments () {
+		if (difference === 0) {
+		  return text('You win!');
+		} else if (difference <= 5) {
+		  return text('Your guess is on fire');
+		} else if (difference <= 10) {
+		  return text('Your guess is very hot');
+		} else if (difference <= 20) {
+		  return text('Your guess is hot');
+		} else if (difference <= 30) {
+		  return text('Your guess is warm');
+		} else if (difference <= 50) {
+		  return text('Your guess is cold');
+		} else if (difference > 50) {
+		  return text('Your guess is ice cold');
+		}
+	};
+	
+
+    var text = function (insert) {
+    	$(feedback).text(insert);
+    };
+
+ 
     newGame();
     //console.log(rightNumber)
     console.log(rightNumber);
@@ -55,6 +91,13 @@ $(document).ready(function () {
     $('form').submit(function(e) {
       e.preventDefault();
       guesses();
+      
+	  if (validation(guess)) {
+	    comments();
+	  } else {
+	  	alert('boom');
+	  	// return true;
+	  }
     });
 
 });
